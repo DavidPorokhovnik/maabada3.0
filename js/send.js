313a3388e5724ed51c941f6ab72b130e6b5cb0c4
David Porokhovnik <dporokhovnik@gmail.com>
2019-06-21 18:08:24 +0300

    17th commit. client: adding to the order form, button 'cancel'[html, css, js-jquery]

--- js/send.js
+++ js/send.js
@@ -3,11 +3,31 @@
 
 $(document).ready(function(){
 
+    let worksCounter = 0;
+
     $(document).on('click', '.work', function () {
-        console.log(this);
+        worksCounter++;
+        if (worksCounter == 1) {
+            $('.order-works').children('.number').remove();
+        }
+        //console.log(this);
         let workName = $(this).children('.work-name').text();
-        console.log(workName);
-        $('.work-field').append(workName);
+        //console.log(workName);
+        let workParagraph = '<p class="work-field">'+ worksCounter + '. ' + workName + '</p>';
+        $('.order-works').append(workParagraph);
     })
+
+    $('.btn-cancel').click(function () {
+        $('.order-works').children().remove();
+        let numberParagraph = '<p class="number">1. </p>';
+        $('.order-works').append(numberParagraph);
+        worksCounter = 0;
+    })
+
+    /*function orderCancel() {
+        $('.order-works').children.remove();
+        let numberParagraph = '<p class="number">1. </p>';
+        $('.order-works').append(numberParagraph);
+    }*/
 
 });
